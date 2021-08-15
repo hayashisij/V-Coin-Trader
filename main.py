cfb0f9492128ea30441180fc317f1d7b5e6b7c5e
hayashi seiji <hayashiseiji1985@gmail.com>
2021-08-15 23:21:48 +0900

    wrapper

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     bitflyer = BitFlyer()
     from_date = datetime.date(year=2021, month=7, day=21)
     chats = bitflyer.get_chats(from_date=from_date)
-    balance = bitflyer.get_balance()
+    coins = bitflyer.get_coin_ins()
     print("end")
 
 if __name__ == '__main__':
