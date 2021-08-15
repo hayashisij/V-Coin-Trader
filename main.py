c7e85042cd01971d171875f49c6e1b14645b0d89
hayashi seiji <hayashiseiji1985@gmail.com>
2021-08-15 22:43:57 +0900

    wrapper

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import datetime
-
 from lib.bitflyer import BitFlyer, BitFlyerError
 import datetime
 
@@ -8,5 +6,6 @@ def main():
     from_date = datetime.date(year=2021, month=7, day=21)
     chats = bitflyer.get_chats(from_date=from_date)
     print("end")
+
 if __name__ == '__main__':
     main()
