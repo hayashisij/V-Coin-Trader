a3598528e87ed3c2bbdd0769df0d3c7d4beccd8a
hayashi seiji <hayashiseiji1985@gmail.com>
2021-08-15 23:45:53 +0900

    wrapper

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     from_date = datetime.date(year=2021, month=7, day=21)
     chats = bitflyer.get_chats(from_date=from_date)
     coins = bitflyer.get_coin_ins()
+    position = bitflyer.get_positions("FX_BTC_JPY")
     print("end")
 
 if __name__ == '__main__':
