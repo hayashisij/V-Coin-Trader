7aa7a3ce92dbf465f0fabbbad42d8c44a94a06d0
hayashi seiji <hayashiseiji1985@gmail.com>
2021-09-12 16:47:51 +0900

    エラー処理を行うクラスを作成

--- lib/public.py
+++ lib/public.py
@@ -1,22 +1,33 @@
 import sys
 import requests
-
 from requests import Response
-from datetime import date, datetime
-from crypt import Crypt
-
-class BitFlyerError(Exception):
-    pass
 
 
-class ApiRequestError(BitFlyerError):
-    pass
-
-
-class RequestParamError(BitFlyerError):
-    pass
+class ApiError(Exception):
+    def ApiRequestError(self, res: response = response):
+        if res is None:
+            print(f"status code: null")
+        else:
+            print(f"status code: {res.status_code}")
 
 
 class PublicAPI:
     def __init__(self):
-        self.url = "https://api.bitflyer.com"
+        self.base_url = "https://api.bitflyer.com"
+
+    def base_get(self, path: str = None, params: dict = None):
+        url: str = self.base_url + path
+        response = requests.get(url=url, params=params)
+
+        if response is None:
+            raise ApiRequestError(response)
+
+        if response.status_code != 200:
+            raise ApiError.ApiRequestError(response)
+
+        return response
+
+    # マーケットの一覧
+    def get_markets(self):
+        path: str = "/v1/getmarkets"
+        return self.base_get(path=path)
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from lib.bitflyer import BitFlyer, BitFlyerError
+from lib.public import PublicAPI
 import datetime
 
+
 def main():
-    bitflyer = BitFlyer()
-    from_date = datetime.date(year=2021, month=7, day=21)
-    chats = bitflyer.get_chats(from_date=from_date)
-    coins = bitflyer.get_coin_ins()
+    pub = PublicAPI()
+    markets = pub.get_markets()
+#   bitflyer = BitFlyer()
+#   from_date = datetime.date(year=2021, month=7, day=21)
+#   chats = bitflyer.get_chats(from_date=from_date)
+#   coins = bitflyer.get_coin_ins()
 
     print("end")
+
 
 if __name__ == '__main__':
     main()
